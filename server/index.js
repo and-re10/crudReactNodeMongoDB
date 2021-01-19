035895865cb5ab4607d0b7b3f2725f43e7c8fa94
and-re10 <andreandre_1234@hotmail.com>
2021-01-19 19:42:38 +0100

    Connection to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+// DATABASE 
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true} ,() => {
+    try {
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+
 // Server
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.listen(3001, () => console.log('Running on port 3001'));
+app.listen(3333, () => console.log('Running on port 3333'));
+
