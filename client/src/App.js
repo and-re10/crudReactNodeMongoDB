299bd8cf9f4e631f22f797688fd11b692a7bacb4
and-re10 <andreandre_1234@hotmail.com>
2021-01-20 12:33:53 +0100

    Création des routes (front-end)

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
 import './styles/App.css';
-import Home from "./pages/home/index";
-import About from "./pages/about/index";
-import OneMovie from "./pages/oneMovie/index";
+import MainRoutes from "./routes/mainRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <About/>
-      <OneMovie/>
-    </div>
+      <MainRoutes/>
   );
 }
 
--- client/src/routes/mainRoutes.js
+++ client/src/routes/mainRoutes.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../pages/home/index";
+import About from "../pages/about/index";
+import OneMovie from "../pages/oneMovie/index";
 
 
 function MainRoutes(){
     return (
         <Routes>
-            
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>}/>
+            <Route path="/:id" element={<OneMovie/>}/>
         </Routes>
     )
 }
+
+export default MainRoutes;
