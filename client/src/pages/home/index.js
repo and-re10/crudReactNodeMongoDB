c999a522f3ce3120829bc6951a6a0e337cdeed55
and-re10 <andreandre_1234@hotmail.com>
2021-01-20 13:00:28 +0100

    Connection a l'api qu'on a crée (tous les movies)

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import axios from 'axios';
 
 function Home(){
+    // hook to store the data from the l'api
+    const [movieReviewList, setMovieReviewList] = useState([]);
+
+    // Connect to the API and recive all the movies
+    useEffect(() => {
+        axios.get('http://localhost:3004/api/movies').then(response => {
+        console.log(response.data);
+        // send the data to the hook movieReviewList
+        setMovieReviewList(response.data);
+        });
+    }, [])
+
     return (
         <div className="App">
             <h1>CRUD APPLICATION</h1>
             <h1>Home</h1>
+            <ul>
+            {movieReviewList.map((data, key) => {
+                return (
+                    <div key={data._id}>
+                        <li>Movie Name: {data.name}</li>
+                        <li>Movie Review: {data.review}</li>
+                    </div>
+                )
+            })}
+            </ul>
         </div>
     );
 };
